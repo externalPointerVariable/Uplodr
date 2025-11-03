0c49f795f6d0fc205cb3105fe0a7c6aa251bc28d
externalPointerVariable <vicky986847@gmail.com>
2025-11-03 04:26:55 +0000

    refactor: Update S3 client initialization to use @aws-sdk/client-s3 and restructure credentials

--- backend/utils/s3.config.js
+++ backend/utils/s3.config.js
@@ -1,11 +1,13 @@
 const multer = require("multer");
 const multerS3 = require("multer-s3");
-const AWS = require("aws-sdk");
+const { S3Client } = require("@aws-sdk/client-s3");
 
-const s3Client = new AWS.S3({
+const s3Client = new S3Client({
   region: process.env.S3_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
 const BUCKET_NAME = process.env.S3_BUCKET_NAME;
